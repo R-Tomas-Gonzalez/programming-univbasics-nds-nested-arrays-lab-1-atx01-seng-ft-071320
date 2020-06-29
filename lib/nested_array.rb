4dad838382134cdbdb7b3d67dfe3c641ef6155f0
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-29 21:34:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,9 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  assembled_matrix[1][1]
+  matrix[1][1]
+  matrix[1][1] = "lemons"
+  matrix[1][1]
   # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
